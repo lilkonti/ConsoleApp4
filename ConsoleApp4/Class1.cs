c40d9716cb464d42cc2220e9ad11e0120a8194bb
Korn_ <Korn_@DESKTOP-HRO6ADI>
2023-04-20 18:57:00 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp4/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Class1
+    {
+        char c;
+        int n1;
+        int n2;
+
+        
+    }
+}
---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Data.Common;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+
+namespace ConsoleApp4
+{
+    public delegate int PerformCalculation(int x, int y);
+    internal class Program
+    {
+        static int Add(int a, int b)
+        {
+            return a + b;
+        }
+        static int Minus(int a, int b)
+        {
+            return a - b;
+        }
+        static int Up(int a, int b)
+        {
+            return a * b;
+        }
+        static int Down(int a, int b)
+        {
+            return a / b;
+        }
+        static void Main(string[] args)
+        {
+            PerformCalculation obj = null;
+            Console.WriteLine("First numder");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Second numder");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("operator\n1 - +\n2 - -\n3 - *\n4 - /");
+            int oper = Convert.ToInt32(Console.ReadLine());
+
+            switch (oper) 
+            {
+                case 1:
+                    obj = Add;
+                    break;
+                case 2:
+                    obj = Minus;
+                    break;
+                case 3:
+                    obj = Up;
+                    break;
+                case 4:
+                    obj = Down;
+                    break;
+                default: 
+                    Console.WriteLine("error");
+                    break;
+            }
+            int ans = obj(num1, num2);
+            Console.WriteLine(ans);
+        }
+        
+    }
+}
